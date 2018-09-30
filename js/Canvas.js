2f0f5163f5427b1ad232499e4c3113f29838aaf2
rhuxell <rhuxell@gmail.com>
2018-09-30 01:35:52 -0300

    Agrego fillrect

--- js/Canvas.js
+++ js/Canvas.js
@@ -1,5 +1,6 @@
 function iniciar(){
     var elemento=document.getElementById('lienzo');
     lienzo= elemento.getContext('2d');
+    lienzo.fillRect(110, 110, 100, 100);
 }
 windows.addEventListener("load", iniciar, false);
